2d16ad1aae208b9319e7451db74cd3d60373f8a3
Gustavo Barreto <gustavo@barreto.xyz>
2023-09-04 20:23:10 -0300

    Primeiro Commit-Projeto Base

---
+++ Escola_POO_BASE/Classes/Aluno.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola_POO_BASE.Classes
+{
+    public class Aluno : Usuario
+    {
+        #region Propriedades
+        public DateTime DtMatricula { get; set; }
+        #endregion
+
+        #region Construtores
+        public Aluno()
+        {
+        }
+
+        public Aluno(int id, string nome, DateTime dtNascimento, DateTime dtMatricula, string email, string senha, bool ativo) : base(id, nome, dtNascimento, email, senha, ativo)
+        {
+            DtMatricula = dtMatricula;
+        }
+        #endregion
+
+        #region Métodos
+
+        public void Cadastrar(List<Aluno> alunos)
+        {
+            string query = string.Format($"INSERT INTO Aluno VALUES('{Nome}','{DtNascimento}','{DtMatricula}','{Email}','{Crypto.Sha256("123")}',1)");
+            query += "; select SCOPE_IDENTITY()I";
+            Conexao cn = new Conexao(query);
+
+            Aluno usuario = new Aluno();
+
+            try
+            {
+                cn.AbrirConexao();
+                Id = Convert.ToInt32(cn.comando.ExecuteScalar());
+                alunos.Add(this);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                cn.FecharConexao();
+            }
+        }
+
+        public void Alterar()
+        {
+            string query = string.Format($"UPDATE Aluno SET Nome = '{Nome}',DtNascimento = '{DtNascimento}' ,DtMatricula = '{DtMatricula}',Email = '{Email}' where Id = 4 ");
+            Conexao cn = new Conexao(query);
+            try
+            {
+                cn.AbrirConexao();
+                cn.comando.ExecuteNonQuery();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally { cn.FecharConexao(); }
+        }
+
+
+        public void Excluir()
+        {
+            string query = string.Format($"update Aluno set ativo = 0 where id = {Id}");
+            Conexao cn = new Conexao(query);
+
+            try
+            {
+                cn.AbrirConexao();
+                cn.comando.ExecuteNonQuery();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                cn.FecharConexao();
+            }
+        }
+
+
+        public static List<Aluno> Buscar(List<Aluno> alunos,int indexCbbBuscar,string texto)
+        {
+            switch (indexCbbBuscar)
+            {
+                case 0:
+                    //Busca por nome
+
+                    return  alunos.Where(a => a.Nome.ToUpper().Contains(texto.ToUpper())).ToList();
+
+                    //breal; quando não for return, é obrigatorio o uso do break
+                    
+                    case 1:
+                    //Busca por Email
+
+                    return alunos.Where(a => a.Email.Contains(texto)).ToList();
+
+
+                    
+                    case 2:
+                    //Busca por Matrícula (Id)
+
+                    return alunos.Where(a => a.Id == Convert.ToInt32(texto)).ToList();
+
+                    
+
+                default:
+                    //Retornar a lista sem filtro
+
+                    return alunos;
+                   
+            }
+        }
+
+
+        #endregion
+    }
+    
+
+
+}
+ 
---
+++ Escola_POO_BASE/Classes/Conexao.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola_POO_BASE.Classes
+{
+    internal class Conexao
+    {
+
+        //conexão remota
+
+        #region Variáveis
+
+        //String de conexão                                            Informações CHUMBADAS - HardCode
+        private static string _strConexao = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=EscolaN22;Integrated Security=True";
+
+        //Variaveis de uso(podem ou não serem usadas ao decorrer do projeto)
+        public SqlConnection conexao = new SqlConnection(_strConexao);
+        public SqlCommand comando; //Armazenar a query
+        public SqlDataAdapter da; //Adaptador para alguns componentes
+        public SqlDataReader dr; //Recebe os select´s
+        public DataSet ds; //trabalha com multiplas tabelas
+
+
+
+        #endregion
+
+        #region Construtores
+
+        public Conexao(string query) 
+        { 
+            comando = new SqlCommand(query, conexao); //comando montado
+            da = new SqlDataAdapter(query, conexao); //caso necessario, esta pronto
+            ds = new DataSet(); //se necessario
+        }
+
+        #endregion
+
+        #region Métodos
+        //um metodo para abrir a conexao com o banco
+
+        public void AbrirConexao()
+        {
+            if (conexao.State == ConnectionState.Open) 
+            {
+                conexao.Close();    
+            } 
+            conexao.Open(); 
+            
+
+        }
+        public void FecharConexao()
+        {
+            if (conexao.State == ConnectionState.Open)
+            {
+                conexao.Close();
+            }
+        }
+
+        #endregion
+    }
+}
